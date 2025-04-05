7cae9072514fcb5c7f4fc309adcd1ee1771f8eed
Arman <pej@pejs-MacBook-Pro.local>
2025-04-05 07:36:28 +0330

    first commit

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,12 +1,13 @@
 import Player from '../Player/Player';
+import Controls from '../Controls/Controls'
 
 export default function Game() {
     return (
       <div className="relative w-full h-screen bg-green-700 flex items-center justify-center">
-        <div className="absolute top-10">
+        <div className="absolute top-5">
           <Player name="Player 1" position="top" />
         </div>
-        <div className="absolute bottom-10">
+        <div className="absolute bottom-20">
           <Player name="You" position="bottom" />
         </div>
         <div className="absolute left-10 top-1/2 -translate-y-1/2">
@@ -14,7 +15,9 @@ export default function Game() {
         </div>
         <div className="absolute right-10 top-1/2 -translate-y-1/2">
           <Player name="Player 3" position="right" />
+          
         </div>
+        <Controls/>
       </div>
     );
 }
