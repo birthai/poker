9af16621ba9b3ff35fc5697dc198fa1196f02b82
Arman <pej@pejs-MacBook-Pro.local>
2025-04-06 08:54:58 +0330

    add table and some changes in pokerTbale

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -2,21 +2,29 @@ import Player from '../Player/Player';
 import Controls from '../Controls/Controls'
 
 export default function Game() {
+    const handleAction = (action) => {
+        console.log(action); // می‌توانید اینجا منطق بازی را اضافه کنید
+    };
+
     return (
-      <div className="relative w-full h-screen bg-green-700 flex items-center justify-center">
-        <div className="absolute top-5">
-          <Player name="Player 1" position="top" />
+      <div className="relative w-full h-screen bg-green-700 flex flex-col items-center justify-center">
+        <div className="flex justify-between w-full">
+          <div className="absolute top-5 left-10">
+            <Player name="Player 1" position="top" />
+          </div>
+          <div className="absolute bottom-20 right-10">
+            <Player name="You" position="bottom" />
+          </div>
         </div>
-        <div className="absolute bottom-20">
-          <Player name="You" position="bottom" />
+        <div className="flex justify-between w-full">
+          <div className="absolute left-10 top-1/2 transform -translate-y-1/2">
+            <Player name="Player 2" position="left" />
+          </div>
+          <div className="absolute right-10 top-1/2 transform -translate-y-1/2">
+            <Player name="Player 3" position="right" />
+          </div>
         </div>
-        <div className="absolute left-10 top-1/2 -translate-y-1/2">
-          <Player name="Player 2" position="left" />
-        </div>
-        <div className="absolute right-10 top-1/2 -translate-y-1/2">
-          <Player name="Player 3" position="right" />
-          
-        </div>
+        <Controls onAction={handleAction} />
       </div>
     );
 }
