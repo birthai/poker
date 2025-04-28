80a5f59476d665cd9a9a9cf8d1ac25ed61a3eeb0
Arman <pej@pejs-MacBook-Pro.local>
2025-04-28 19:58:06 +0330

    change table and players styles

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -15,7 +15,7 @@ export default function Controls({ onAction }: ControlsProps) {
       // </div>
       <div style={{width:'100%',height:80,position:'absolute',bottom:0,display:'flex',justifyContent:'center',alignItems:'center'}}>
         <div style={{width:'95%',height:'70%',display:'flex',justifyContent:'center',alignItems:'center',backgroundColor:'#546E7A',borderRadius:12}}>
-          <button onClick={() => onAction('fold')} className="btn">Fold</button>
+          <button onClick={() => onAction('fold')} className="btn" style={{backgroundColor:'red'}}>Fold</button>
           <button onClick={() => onAction('check')} className="btn">Check</button>
           <button onClick={() => onAction('raise')} className="btn">Raise</button>
         </div>
