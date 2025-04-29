c2392273473b611f2f5ae17453b12cbdd74015c5
Arman <pej@pejs-MacBook-Pro.local>
2025-04-29 17:44:42 +0330

    some changes in styles

--- src/App.tsx
+++ src/App.tsx
@@ -10,22 +10,14 @@ const App: React.FC = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center overflow-hidden relative">
       
-      {/* Div برای تصویر پس‌زمینه */}
       <div className="absolute inset-0 w-full h-full">
-        {/* <img 
-          src={backgroundImage} 
-          alt="Poker Table"
-          className="absolute inset-0 w-full h-full object-contain"
-        /> */}
-        
-        {/* PokerTable در بالای تصویر */}
+
         <PokerTable 
           players={gameState.players}
           communityCards={gameState.communityCards}
         />
       </div>
 
-      {/* Div برای کنترل‌ها */}
       <div className="relative z-10" style={{ width: '100%', position:'absolute',bottom:0}}>
         {gameState.players[gameState.currentPlayer].isHuman && (
           <Controls onAction={handlePlayerAction} />
--- src/components/Table/PokerTable.tsx
+++ src/components/Table/PokerTable.tsx
@@ -1,94 +1,9 @@
-// import React from "react";
-// import Player from "../Player/Player";
-// import Card from "../Card/Card";
-// import UserProfile from '../../assets/profile.jpg';
-// import backgroundImage from '../../assets/background.svg';
-
-
-// interface CardType {
-//   suit: string;
-//   value: string;
-// }
-
-// interface PlayerType {
-//   name: string;
-//   cards: CardType[];
-//   profile: string;
-// }
-
-// const PokerTable: React.FC = () => {
-//     const players: PlayerType[] = [
-//         { name: "Player 1", cards: [{ suit: "♠", value: "A" },{ suit: "♠", value: "A" }], profile: UserProfile },
-//         { name: "Player 2", cards: [{ suit: "♠", value: "A" },{ suit: "♠", value: "A" }], profile: UserProfile },
-//         { name: "Player 3", cards: [{ suit: "♠", value: "A" },{ suit: "♠", value: "A" }], profile: UserProfile },
-//         { name: "Player 4", cards: [{ suit: "♠", value: "A" },{ suit: "♠", value: "A" }], profile: UserProfile },
-//     ];
-
-//     const communityCards: CardType[] = [
-//         { suit: "♠", value: "A" },
-//         { suit: "♥", value: "K" },
-//         { suit: "♦", value: "Q" },
-//         { suit: "♣", value: "J" },
-//         { suit: "♠", value: "10" }
-//     ];
-
-//     return (
-//         <div 
-//             className="w-full h-full flex items-center justify-center relative" 
-//             style={{ 
-//                 backgroundImage: `url(${backgroundImage})`, 
-//                 backgroundSize: '100% auto',
-//                 backgroundPosition: 'center',
-//                 backgroundRepeat: 'no-repeat',
-//                 overflow: 'visible',
-//                 backgroundPosition: 'center',
-//                 backgroundRepeat: 'no-repeat'
-//             }}
-//         >
-//             {/* Player 1 - Top */}
-//             <div style={{ position: 'absolute', left: '0%', top: '50%', transform: 'translateY(-50%)' }}>
-//                 <Player name={players[0].name} cards={players[0].cards} profile={players[0].profile} position="left" />
-//             </div>
-
-//             {/* Player 2 - Bottom Left */}
-//             <div style={{ position: 'fixed', right: '30%', bottom: '3%' , transform: 'translateY(-50%)' }}>
-//                 <Player name={players[1].name} cards={players[1].cards} profile={players[1].profile} position="bottom" />
-//             </div>
-
-//             {/* Player 3 - Bottom Right */}
-//             <div style={{ position: 'absolute', left: '30%', bottom: '3%' , transform: 'translateY(-50%)' }}>
-//                 <Player name={players[2].name} cards={players[2].cards} profile={players[2].profile} position="bottom" />
-//             </div>
-
-//             {/* Player 4 - Center Right */}
-//             <div style={{ position: 'absolute', right: '0%', top: '50%', transform: 'translateY(-50%)' }}>
-//                 <Player name={players[3].name} cards={players[3].cards} profile={players[3].profile} position="right" />
-//             </div>
-
-//             {/* Community Cards - Center */}
-//             <div style={{
-//                 height: '10%',
-//                 width: '24%',
-//                 position: 'absolute',
-//                 justifyContent: 'center',
-//                 alignItems: 'center',
-//                 display: 'flex',
-//                 flexDirection: 'row'
-//             }}>
-//                 {communityCards.map((card, idx) => (
-//                     <Card key={idx} suit={card.suit} value={card.value} />
-//                 ))}
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default PokerTable;
 
 import React from 'react';
 import Player from '../Player/Player';
 import Card from '../Card/Card';
 import './PokerTable.css';
+import backgroundImage from '../../assets/profile.jpg';
 
 const communityCards = [
   { rank: '8', suit: '♦️' },
@@ -99,15 +14,17 @@ const communityCards = [
 ];
 
 const players = [
-  { name: 'Tyler', balance: 235 },
-  { name: 'Anthony', balance: 21 },
-  { name: 'Kaylee', balance: 178 },
-  { name: 'Michael', balance: 127 },
-  { name: 'Daniel', balance: 54 },
-  { name: 'Victoria', balance: 340 },
-  { name: 'Joseph', balance: 112 },
-  { name: 'Amelia', balance: 49 },
+  { name: 'Tyler', balance: 235 ,profileImage:backgroundImage},
+  { name: 'Anthony', balance: 21 ,profileImage:backgroundImage},
+  { name: 'Kaylee', balance: 178 ,profileImage:backgroundImage},
+  { name: 'Michael', balance: 127 ,profileImage:backgroundImage},
+  { name: 'Daniel', balance: 54 ,profileImage:backgroundImage},
+  { name: 'Victoria', balance: 340 ,profileImage:backgroundImage},
+  { name: 'Joseph', balance: 112 ,profileImage:backgroundImage},
+  { name: 'Amelia', balance: 49,profileImage:'https://placehold.co/60x40'},
 ];
+
+
 
 const PokerTable = () => {
   return (
@@ -124,7 +41,7 @@ const PokerTable = () => {
         <div className="players">
           {players.map((player, index) => (
             <div key={index} className={`player player-${index}`}>
-              <Player name={player.name} balance={player.balance} />
+              <Player name={player.name} balance={player.balance} profileImage={player.profileImage} />
             </div>
           ))}
         </div>
